b8ef533f497cc0b012150b73f3311b6db5418976
CHANG CAI LEE <116559988+caileechang@users.noreply.github.com>
2025-06-10 19:04:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,8 +57,8 @@ def main():
     plot_option = st.sidebar.radio("Choose a visualization:", [
         "Monthly Trends by Cluster",
         "Yearly Fish Landing Summary",
-        "Yearly FIsh  Cluster Trends",
-        "Yearly Elbow & Silhouette",
+       
+        
         "2D KMeans Scatter",
         "3D KMeans Clustering",
         "DBSCAN Anomaly Detection"
