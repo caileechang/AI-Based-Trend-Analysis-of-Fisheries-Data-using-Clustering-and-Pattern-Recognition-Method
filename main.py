52fc5b08007f74ad823d2ecf25298af91193aba7
CHANG CAI LEE <116559988+caileechang@users.noreply.github.com>
2025-10-26 11:29:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,8 @@ def main():
 
    
 
-   
+       # --- Recalculate merged dataset AFTER upload ---
+    merged_df = prepare_yearly(df_land, df_vess)
     
     st.sidebar.header("Select Visualization")
     plot_option = st.sidebar.radio("Choose a visualization:", [
@@ -141,7 +142,7 @@ def main():
         merged['Total Fish Landing (Tonnes)'] = merged['Freshwater (Tonnes)'] + merged['Marine (Tonnes)']
         return merged
 
-    merged_df = prepare_yearly(df_land, df_vess)
+    #===merged_df = prepare_yearly(df_land, df_vess)===
 
     if plot_option == "Monthly Trends by Cluster":
         monthly = df_land.groupby(['Year', 'Month'])['Fish Landing (Tonnes)'].sum().reset_index()
