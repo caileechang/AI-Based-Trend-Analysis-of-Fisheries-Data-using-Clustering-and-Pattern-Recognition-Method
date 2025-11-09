1500fca1c49617f2a5d4859780fb107090f4da1c
CHANG CAI LEE <116559988+caileechang@users.noreply.github.com>
2025-11-10 00:24:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -726,7 +726,7 @@ def main():
             st.markdown("### Outlier Details")
             st.dataframe(outlier_details)
 
-   elif plot_option == "Hierarchical Clustering":
+elif plot_option == "Hierarchical Clustering":
         import scipy.cluster.hierarchy as sch
         from sklearn.cluster import AgglomerativeClustering
         from sklearn.preprocessing import StandardScaler
