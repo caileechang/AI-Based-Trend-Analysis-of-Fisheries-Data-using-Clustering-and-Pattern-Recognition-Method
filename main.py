4c9770b0c3ea9de0e3f2f4296cdd39674a172455
Cai Lee Chang <changcailee0414@gmail.com>
2025-12-13 16:53:06 +0800

    up

--- main.py
+++ main.py
@@ -885,7 +885,7 @@ def main():
 
                 card_html = f"""
                 <div style="
-                    background: radial-gradient(circle at top left, rgba(173, 216, 230, 0.85));
+                    background: radial-gradient(circle at top left, rgba(96, 165, 250, 0.85));
                     border-radius: 14px;
                     padding: 18px 18px 14px 18px;
                     border: 1px solid rgba(0,255,255,0.35);
