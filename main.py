ac19fb4c33a5876892a73b4726831f8b6da9c8f6
Cai Lee Chang <changcailee0414@gmail.com>
2025-12-17 12:25:05 +0800

    up

--- main.py
+++ main.py
@@ -839,8 +839,8 @@ def main():
 
     #st.title("Fisheries Clustering & Pattern Recognition Dashboard")
 
-    import streamlit as st
-    import pandas as pd
+    
+    
 
     st.set_page_config(layout='wide')
     
